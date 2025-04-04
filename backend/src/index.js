e47896cbf4c338c604ca8f1a15323b3bb17cae73
Aylon <aylonc1@gmail.com>
2025-04-04 20:30:30 +0300

    added mongo as db

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
@@ -10,6 +12,14 @@ app.post("/api/notifications/email", (req, res) => {
     console.log(`Email to: ${to}, subject: ${subject}, message: ${message}`);
     res.status(200).json({ message: "Email sent (simulated)" });
 });
+console.log(process.env.MONGO_URI)
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('✅ Connected to MongoDB'))
+  .catch((err) => console.error('❌ MongoDB connection error:', err));
 
 app.listen(3001, () => {
     console.log("Backend running on http://localhost:3001");
