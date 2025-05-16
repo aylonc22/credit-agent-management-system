a38ce6f7426ce00f7307f6905713346268578aa3
Aylon <aylonc1@gmail.com>
2025-05-16 13:49:52 +0300

    minor flow bug fix

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import './index.scss';
 import backArrow from '../../assets/images/icons/arrow-back.svg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
+const Header = ({ flag }) => {
+  const location = useLocation();
 
-//flag is for flagging the need for a real header of for the authentication header
-const Header = ({flag})=>{
-   
-    if(flag){
-        return (<header className="header header--fixed">
-                <div className="header__inner">
-                  <div className="header__icon">
-                    <Link to="/login"><img src={backArrow} alt="Back" /></Link>
-                  </div>
-                </div>
-              </header>);
-    }else{
-        return(
-            <header className="header header--fixed">	
-                <div className="header__inner">	
-                    <div className="header__logo header__logo--text"><a href="index.html">Pay<strong>glow</strong></a></div>	
-                    <div className="header__icon open-panel" data-panel="left"><img src="images/icons/user.svg" alt="image" title="image"/></div>
-                </div>
-            </header> )
-    }
-}
+  // If on /login, redirect back arrow to /landing instead of /login
+  const backLink = location.pathname === '/forgot-password' ? '/login' : '/landing';
+
+  if (flag) {
+    return (
+      <header className="header header--fixed">
+        <div className="header__inner">
+          <div className="header__icon">
+            <Link to={backLink}>
+              <img src={backArrow} alt="Back" />
+            </Link>
+          </div>
+        </div>
+      </header>
+    );
+  } else {
+    return (
+      <header className="header header--fixed">
+        <div className="header__inner">
+          <div className="header__logo header__logo--text">
+            <a href="index.html">
+              Pay<strong>glow</strong>
+            </a>
+          </div>
+          <div className="header__icon open-panel" data-panel="left">
+            <img src="images/icons/user.svg" alt="image" title="image" />
+          </div>
+        </div>
+      </header>
+    );
+  }
+};
 
 export default Header;
