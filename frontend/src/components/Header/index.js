9cc52eac02639933eacacce736d9ba059c441dc7
Aylon <aylonc1@gmail.com>
2025-05-16 15:18:05 +0300

    finished dashboard

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
 import backArrow from '../../assets/images/icons/arrow-back.svg';
 import user from '../../assets/images/icons/user.svg';
@@ -6,38 +6,54 @@ import { Link, useLocation } from 'react-router-dom';
 
 const Header = ({ flag }) => {
   const location = useLocation();
+  const [scrolled, setScrolled] = useState(false);
 
-  // If on /login, redirect back arrow to /landing instead of /login
+  useEffect(() => {
+    const contentEl = document.querySelector('.page__content');
+
+    if (!contentEl) return;
+
+    const handleScroll = () => {
+      const windowTop = contentEl.scrollTop;
+      const bodyTop = document.body.offsetTop;
+
+      setScrolled(windowTop > bodyTop);
+    };
+
+    contentEl.addEventListener('scroll', handleScroll);
+    handleScroll(); // initial check on mount
+
+    return () => contentEl.removeEventListener('scroll', handleScroll);
+  }, []);
+
   const backLink = location.pathname === '/forgot-password' ? '/login' : '/landing';
 
-  if (flag) {
-    return (
-      <header className="header header--fixed">
-        <div className="header__inner">
+  const headerClass = `header header--fixed${scrolled ? ' header--page' : ''}`;
+
+  return (
+    <header className={headerClass}>
+      <div className="header__inner">
+        {flag ? (
           <div className="header__icon">
             <Link to={backLink}>
               <img src={backArrow} alt="Back" />
             </Link>
           </div>
-        </div>
-      </header>
-    );
-  } else {
-    return (
-      <header className="header header--fixed">
-        <div className="header__inner">
-          <div className="header__logo header__logo--text">
-            <a href="index.html">
-              Pay<strong>glow</strong>
-            </a>
-          </div>
-          <div className="header__icon open-panel" data-panel="left">
-            <img src={user} alt="image" title="image" />
-          </div>
-        </div>
-      </header>
-    );
-  }
+        ) : (
+          <>
+            <div className="header__logo header__logo--text">
+              <a href="index.html">
+                Pay<strong>glow</strong>
+              </a>
+            </div>
+            <div className="header__icon open-panel" data-panel="left">
+              <img src={user} alt="image" title="image" />
+            </div>
+          </>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
